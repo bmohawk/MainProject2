c7ace678f36c1303478578ed02ae59a25d3d44bd
Brett Namba <brett@mohawkgp.com>
2021-07-12 13:54:47 -0700

    Adding Lib1 as ref to Main2

--- MainProject2/Program.cs
+++ MainProject2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Lib1;
 using Lib2;
 
 namespace MainProject2
@@ -7,6 +8,8 @@ namespace MainProject2
     {
         static void Main(string[] args)
         {
+            var lib1 = new Lib1Class1();
+            Console.WriteLine($"Lib1 DoWork(): {lib1.DoWork()}");
             var lib2 = new Lib2Class1();
             Console.WriteLine($"Lib2 DoWork(): {lib2.DoLib2Work()}");
         }
