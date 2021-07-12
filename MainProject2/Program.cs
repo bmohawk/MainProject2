0d7750899ed65caeb3284db5c2cbf2e5c46b367b
Brett Namba <brett@mohawkgp.com>
2021-07-12 11:42:25 -0700

    Initial project

---
+++ MainProject2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MainProject2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
