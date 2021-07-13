692c5b9bd5b492a24d99908f7bab39a542aca0d1
Brett Namba <brett@mohawkgp.com>
2021-07-13 10:31:21 -0700

    Removing Lib2

--- MainProject2/Program.cs
+++ MainProject2/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Lib1;
-using Lib2;
+//using Lib2;
 
 namespace MainProject2
 {
@@ -10,8 +10,8 @@ namespace MainProject2
         {
             var lib1 = new Lib1Class1();
             Console.WriteLine($"Lib1 DoWork(): {lib1.DoWork()}");
-            var lib2 = new Lib2Class1();
-            Console.WriteLine($"Lib2 DoWork(): {lib2.DoLib2Work()}");
+            // var lib2 = new Lib2Class1();
+            // Console.WriteLine($"Lib2 DoWork(): {lib2.DoLib2Work()}");
         }
     }
 }
